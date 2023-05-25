5063d0c53531235d16c125e25e095db18a6f178a
alifrknak@gmail.com <alifrknak@gmail.com>
2023-05-25 12:21:30 +0300

    update

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,12 +1,21 @@
 ﻿using DataStructures;
 
 Trie a = new Trie();
-
-
-
 a.Add("ali");
+a.Add("aliye");
 a.Add("veli");
+a.Add("vali");
+a.Add("fatma");
+a.Add("fatih");
+a.Add("samet");
+a.Add("veliye");
 
+
+Console.WriteLine("deleted  "+a.Remove(" "));
+Console.WriteLine("deleted  "+a.Remove("alii"));
+Console.WriteLine("deleted  "+a.Remove("vli"));
+Console.WriteLine("deleted  "+a.Remove("fat")); //hata  veliyor
+Console.WriteLine("deleted  "+a.Remove("fa"));
 a.Print(a);
-Console.WriteLine();
+
 
--- DataStructures/Trie.cs
+++ DataStructures/Trie.cs
@@ -12,7 +12,7 @@
 		{
 			if (!current.Children.TryGetValue(c, out var child))
 				current.Children.Add(c, child = new Trie());
-			current = child;e
+			current = child;
 		}
 		current.IsEndOfWord = true;
 	}
@@ -25,22 +25,43 @@
 				return false;
 		return current.IsEndOfWord;
 	}
-	
+	public bool Remove(string word)
+	{
+		Trie current = this;
+		char delete = word[0];
+		Trie temp = this;
+
+		for (int i = 0; i < word.Length; i++)
+		{
+			if (!current.Children.TryGetValue(word[i], out current))
+				return false;
+
+			if ((current.Children.Count > 1) && (i == word.Length - 1 || current.IsEndOfWord))
+			{
+				delete = word[i + 1];
+				temp = current;
+			}
+		}
+
+		if (!current.IsEndOfWord)
+			return false;
+		if (current.Children.Count > 0)
+		{
+			current.IsEndOfWord = false;
+			return true;
+		}
+		return temp.Children.Remove(delete);
+
+	}
+
 	public void Print(Trie trie)
 	{
 		if (trie is null)
-		{
 			return;
-		}
 		foreach (var i in trie.Children.Keys)
-		{
 			Console.Write(i + " ");
-		}
 		Console.WriteLine();
-        foreach (var i in trie.Children.Values)
-		{
+		foreach (var i in trie.Children.Values)
 			Print(i);
-		}
-
 	}
 }
