c74f08143c8f67e6e4b56fab4e9b0577da2b8b5b
alifrknak@gmail.com <alifrknak@gmail.com>
2023-05-26 09:52:11 +0300

    update

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,26 +6,6 @@ class Program
 
 	static void Main(string[] args)
 	{
-		Trie a = new Trie();
-		
-		a.Add("ali");
-		a.Add("aliye");
-		a.Add("veli");
-		a.Add("vali");
-		a.Add("fatma");
-		a.Add("fatih");
-		a.Add("samet");
-		a.Add("veliye");
-		a.Add("alim");
-
-        Console.WriteLine(a.Remove("ali"));
-        Console.WriteLine(a.Contains("ali"));
-        Console.WriteLine(a.Contains("fat"));
-        Console.WriteLine(a.Contains("sam"));
-        Console.WriteLine(a.Contains("al"));
-        Console.WriteLine(a.Contains("alii"));
-        Console.WriteLine(a.Contains("al "));
-        Console.WriteLine(a.Contains("va"));
-
+	
     }
 }
