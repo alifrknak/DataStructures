abc9c93edee447312dae496828fd9e97232ae213
alifrknak@gmail.com <alifrknak@gmail.com>
2023-05-29 12:14:17 +0300

    update

---
+++ DataStructures/Link.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+public class Link<T>: IEnumerable<T>
+{
+    public class Node
+    {
+        public T Value { get; set; }
+        public Node? Previous { get; set; }
+        public Node? Next { get; set; }
+        public Node(T item) =>  Value = item;
+    }
+
+    public Node? First { get => _first; }
+    public Node? Last { get => _last;  }
+    public int Count { get => _count; }
+
+    private  int _count;
+    private Node? _first;
+    private Node? _last;
+    private Node? _head;
+    private Node? _tail;
+    public Node AddLast(T item)
+    {
+        if (_head is null)
+        {
+            _head = new Node(item);
+            _tail = _head;
+            return _head;
+        }
+        Node? temp = _tail;
+		_tail.Next = new Node(item);
+        _tail = _tail.Next;
+        _tail.Previous = temp;
+        return _tail;
+    }
+
+	public IEnumerator<T> GetEnumerator()
+	{
+		Node? temp = _head;
+		while (temp is not null)
+		{
+			yield return temp.Value;
+			temp = temp.Next;
+		}
+	}
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+        Node? temp = _head;
+        while (temp is not null)
+        {
+            yield return temp.Value;
+            temp = temp.Next;
+        }
+	}
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DataStructures;
+using System.IO.Compression;
 using System.Security.Cryptography;
 
 class Program
@@ -6,6 +7,22 @@ class Program
 
 	static void Main(string[] args)
 	{
-	
+		LinkedList<int> a = new();
+		
+		a.AddLast(1);
+	 var  r=	a.AddLast(20);
+		a.AddLast(3);
+		a.AddLast(34);
+
+		Link<int> aa = new Link<int>();
+
+		aa.AddLast(1);
+		aa.AddLast(10);
+		aa.AddLast(4);
+
+        foreach (var item in aa)
+        {
+            Console.Write(item+" ");
+        }
     }
 }
