bb967e203c9024a26193db1a61f8126c92b644a1
alifrknak@gmail.com <alifrknak@gmail.com>
2023-05-31 19:05:20 +0300

    update

--- DataStructures/Link.cs
+++ DataStructures/Link.cs
@@ -1,41 +1,69 @@
 ﻿using System.Collections;
 
-public class Link<T>: IEnumerable<T>
+public class Link<T> : IEnumerable<T>
 {
-    public class Node
-    {
-        public T Value { get; set; }
-        public Node? Previous { get; set; }
-        public Node? Next { get; set; }
-        public Node(T item) =>  Value = item;
-    }
+	public class Node
+	{
+		public T Value { get; set; }
+		public Node? Previous { get; set; }
+		public Node? Next { get; set; }
+		public Node(T item) => Value = item;
+	}
 
-    public Node? First { get => _first; }
-    public Node? Last { get => _last;  }
-    public int Count { get => _count; }
+	public Node? First { get => _head; }
+	public Node? Last { get => _last; }
+	public int Count { get => _count; }
 
-    private  int _count;
-    private Node? _first;
-    private Node? _last;
-    private Node? _head;
-    private Node? _tail;
-    public Node AddLast(T item)
-    {
-        if (_head is null)
-        {
-            _head = new Node(item);
-            _tail = _head;
-            _first = _head;
-            return _head;
-        }
-        Node? temp = _tail;
-		_tail.Next = new Node(item);
-        _tail = _tail.Next;
-        _tail.Previous = temp;
-        _last = _tail; 
-        return _tail;
-    }
+	private int _count;
+	private Node? _head;
+	private Node? _tail;
+	private Node? _last;
+	public Node AddLast(T item)
+	{
+		if (_head is null)
+		{
+			_head = new Node(item);
+			_tail = _head;
+			_last = _head;
+			return _head;
+		}
+		Node? temp = _tail;
+		_tail = _tail.Next;
+		_tail = new Node(item);
+		_tail.Previous = temp;
+		temp.Next = _tail;
+		_last = _tail;
+		return _tail;
+	}
 
+	public bool Remove(T item)
+	{
+		Node? temp = _head;
+		if (temp is null)
+			return false;
+		if (temp.Value.Equals(item))
+		{
+			_head = _head.Next;
+			_head.Previous = null;
+			return true;
+		}
+		while (temp.Next is not null && !temp.Next.Value.Equals(item))
+			temp = temp.Next;
+		if (temp.Next is null)
+			return false;
+
+		if (temp.Next.Next is null)
+		{
+			temp.Next = null;
+			_last = temp;
+			return true;
+		}
+
+		temp.Next = temp.Next.Next;
+		temp.Next.Previous = temp;
+
+		return true;
+	}
 	public IEnumerator<T> GetEnumerator()
 	{
 		Node? temp = _head;
@@ -48,11 +76,11 @@ public class Link<T>: IEnumerable<T>
 
 	IEnumerator IEnumerable.GetEnumerator()
 	{
-        Node? temp = _head;
-        while (temp is not null)
-        {
-            yield return temp.Value;
-            temp = temp.Next;
-        }
+		Node? temp = _head;
+		while (temp is not null)
+		{
+			yield return temp.Value;
+			temp = temp.Next;
+		}
 	}
 }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,21 +8,14 @@ class Program
 	static void Main(string[] args)
 	{
 		LinkedList<int> a = new();
-		
 		a.AddLast(1);
-	 var  r=	a.AddLast(20);
+		var r = a.AddLast(20);
 		a.AddLast(3);
 		a.AddLast(34);
 
 		Link<int> aa = new Link<int>();
 
-		aa.AddLast(1);
-		aa.AddLast(10);
-		aa.AddLast(4);
 
-        Console.WriteLine(aa.First.Value);
-        Console.WriteLine(aa.Last.Value);
-        Console.WriteLine(aa.Count);
-
-    }
+	
+	}
 }
