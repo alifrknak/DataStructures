f83d723b4f4499090851fc077ec968203a30e486
alifurkan <alifrknak@gmail.com>
2023-09-13 18:19:52 +0300

    added hashset

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,6 +7,31 @@ partial class Program
 {
 	static void Main(string[] args)
 	{
-        Console.WriteLine();
-    }
+		Console.WriteLine();
+		new System.Collections.Generic.HashSet<int>();
+	}
+}
+public class HashSet
+{
+	public int Count { get; private set; }
+	public int Capacity { get; private set; } = 20;
+
+	private int[] table;
+
+	public HashSet(int capacity)
+	{
+		table = new int[capacity];
+		Capacity = capacity;
+	}
+	public HashSet()
+	{
+		table = new int[Capacity];
+	}
+
+	private int Hash(int key) => key % Capacity;
+
+	public bool Add(int item)
+	{
+
+	}
 }
