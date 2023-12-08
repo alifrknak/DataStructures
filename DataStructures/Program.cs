7216c5d981cf601dd036caaaec76289505d98d55
alifurkan <alifrknak@gmail.com>
2023-12-08 10:02:00 +0300

    edit

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -11,27 +11,4 @@ partial class Program
 		new System.Collections.Generic.HashSet<int>();
 	}
 }
-public class HashSet
-{
-	public int Count { get; private set; }
-	public int Capacity { get; private set; } = 20;
-
-	private int[] table;
-
-	public HashSet(int capacity)
-	{
-		table = new int[capacity];
-		Capacity = capacity;
-	}
-	public HashSet()
-	{
-		table = new int[Capacity];
-	}
-
-	private int Hash(int key) => key % Capacity;
-
-	public bool Add(int item)
-	{
-
-	}
-}
+ 
